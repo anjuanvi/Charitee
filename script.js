20a8aebb3f299f25f44ec6f369fc69811be4e907
anju <anjusha.prakash123@gmail.com>
2023-10-26 16:38:53 +0530

    charitee

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const animatedText = document.querySelector(".moving-text");
+    animatedText.style.animationPlayState = "running";
+});
